d01f457176734401b49f997c8a96797aa1340940
Ross Briden <rcbridendev@gmail.com>
2015-08-08 00:16:54 -0400

    Initial commit

---
+++ src/com/rostifar/scrabbleproject/GameBoard.java
@@ -0,0 +1,51 @@
+package com.rostifar.scrabbleproject;
+
+
+import java.util.Arrays;
+
+/**
+ * Created by ross on 7/22/15.
+ */
+public class GameBoard {
+
+    private static final int ROWS = 15;
+    private static final int COLUMNS = 15;
+
+
+    public void setup_Grid() {
+
+        int[][] grid = new int[15][15];
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
