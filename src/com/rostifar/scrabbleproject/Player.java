2a8553109e1c6d09d0727499346014c05c8915d2
GitLazy <lazycal@gmail.com>
2015-10-18 01:13:52 -0400

    Added some attributes and minor behavior. Changed voids to booleans where applicable.

--- src/com/rostifar/scrabbleproject/Player.java
+++ src/com/rostifar/scrabbleproject/Player.java
@@ -4,15 +4,38 @@ package com.rostifar.scrabbleproject;
  * Created by D14048 on 10/4/2015.
  */
 public class Player {
+    private Rack rack;
+    private String name;
+    private int turnCounter;
 
+    public Player(String aName) {
+        name = aName;
+        turnCounter = 0;
+        rack = new Rack();
+    }
 
-    public void getName(){}
+    public String getName() {
+        return name;
+    }
 
-    public void hasRack(){}
+    public boolean hasRack(){
+        return rack != null;
+    }
 
-    public void isFirstTurn(){}
+    public Rack getRack() {
+        return rack;
+    }
 
-    public void takeTurn(){}
+    public boolean isFirstTurn() {
+        return turnCounter == 0;
+    }
 
+    public void takeTurn() {
+
+    }
+
+    public String toString() {
+        return "Player " + getName() + " - " + getRack();
+    }
 
 }
