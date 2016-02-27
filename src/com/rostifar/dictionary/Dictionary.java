6d9d493765bf8ec19c35081c683880811d0c08c7
Cal <cal.briden@wellsfargo.com>
2016-02-27 00:34:15 -0500

    Attempt to get all the dictinary changes merged into origin

--- src/com/rostifar/dictionary/Dictionary.java
+++ src/com/rostifar/dictionary/Dictionary.java
@@ -1,10 +1,12 @@
 package com.rostifar.dictionary;
 
+import com.rostifar.gamecontrol.ScrabbleGameException;
+
 /**
  * Defines the public contract for use of a dictionary.
  * Created by GitLazy (Dad) on 12/11/2015.
  */
 public interface Dictionary {
-    DictionaryLookupResult lookupWord(String lookupWord) throws Exception;
+    DictionaryLookupResult lookupWord(String lookupWord) throws ScrabbleGameException;
     //boolean isValidWord(DictionaryLookupResult dictionaryLookupResult);
 }
