e83bb9a13b2e239f8909be96d539a15628e039db
GitLazy <Rossey2003>
2016-03-20 09:04:57 -0400

    Working on

--- src/com/rostifar/servlets/ScrabbleGameControllerServlet.java
+++ src/com/rostifar/servlets/ScrabbleGameControllerServlet.java
@@ -1,10 +1,10 @@
 package com.rostifar.servlets;
 
-import com.rostifar.gamecontrol.GameManager;
-import com.rostifar.gamecontrol.ScrabbleGameException;
-import com.rostifar.gamecontrol.ScrabbleGameManager;
+import com.rostifar.scrabbleproject.Player;
 
-import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 /**
@@ -12,18 +12,35 @@ import java.io.IOException;
  *  Created by GitLazy (Dad) on 3/4/2016.
  */
 public class ScrabbleGameControllerServlet extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         System.out.println(this.getClass().getName() + "DO GET Called!");
-
+        String action = request.getParameter("Action");
+        //JSObject
     }
 
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         System.out.println(this.getClass().getName() + "Starting Scrabble Game Engine on Backend...");
-        try {
-            GameManager gameManager = new ScrabbleGameManager();
-            gameManager.runGame();
-        } catch (ScrabbleGameException gameExp) {
-            throw new ServletException(gameExp);
-        }
+
+            // GameManager gameManager = new ScrabbleGameManager();
+            Player aPlayer = getPlayerFromSession(request);
+            //gameManager.runGame();
+
     }
+
+    protected Player getPlayerFromSession(HttpServletRequest request) {
+        // GameManager gameManager = new ScrabbleGameManager();
+        HttpSession session = request.getSession();
+        Player aPlayer;
+        aPlayer = (Player) session.getAttribute("Player");
+        //gameManager.runGame();
+        return aPlayer;
+    }
+
+    protected void storePlayerOnSession(HttpServletRequest request, Player aPlayer) {
+        // GameManager gameManager = new ScrabbleGameManager();
+        HttpSession session = request.getSession();
+        session.setAttribute("Player", aPlayer);
+    }
+
+
 }
