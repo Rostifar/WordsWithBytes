f99971c4207ade231d950bb97d861195b6e3e7e4
GitLazy <Rossey2003>
2016-07-19 16:39:45 -0400

    Fixed FileBasedDictionary. Changed text dictionary file to use same dictionary as used by word game example.

--- src/com/rostifar/dictionary/FileBasedDictionary.java
+++ src/com/rostifar/dictionary/FileBasedDictionary.java
@@ -2,46 +2,45 @@ package com.rostifar.dictionary;
 
 import com.rostifar.gamecontrol.ScrabbleGameException;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
-import java.util.Scanner;
+import java.io.InputStreamReader;
 
 /**
  * Implement a dictionary as a flat file lookup.
- * File is in the format of a
- * 1) Header on the first line,
- * 2) All other lines | (pipe) delimited with a unique ID as the first field and the word as the second field.
- * ID|WORD <-- header on first line
- * 120|abalones <-- examples of records in rest of files
- * 166|abashments
- * etc....
- * This same file and format is used to load the database for a databased dictionary implementation.
+ * File is in the format of asingle record with each word delimited by a space
  * Created by GitLazy (Dad) on 12/11/2015.
  */
 public class FileBasedDictionary extends AbstractDictionary implements Dictionary {
-    private static final String FILE_PATH = "dictionary.dsv";
-    private static final char FIELD_DELIMITER = '|';
+    private static final String FILE_PATH = "dictionary.txt";
     InputStream inputStream;
+    static StringBuilder cache;
 
-    /**
-     * Make default constructor private to force use of DictionaryFactory
-     */
-   /* FileBasedDictionary(){
-        initialize();
-    }*/
-
-    @Override
-    protected void finalize() throws Throwable {
-        super.finalize();
-        inputStream.close();
-    }
 
     /**
      * Initialize the Dictionary. Required by subclasses of AbstractDictionary
      * @return fully initialized instance of a dictionary, ready for use
      */
     public Dictionary initialize() throws Exception {
+        if (cache != null) {
+            return this;
+        }
+
+        cache = new StringBuilder();
         ClassLoader classLoader = getClass().getClassLoader();
         inputStream = classLoader.getResourceAsStream(FILE_PATH);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+            //int readSoFar = inputStream.read(buffer, 0, 4096);
+
+            /*if (readSoFar == -1)
+                break;*/
+
+            cache.append(line);
+        }
+
         return this;
     }
 
@@ -50,28 +49,15 @@ public class FileBasedDictionary extends AbstractDictionary implements Dictionar
      * @param aWord does this word exist in the dictionary?
      * @return true if yes, false if no
      */
-
     public DictionaryLookupResult searchDictionary(String aWord) throws ScrabbleGameException {
 
         DictionaryLookupResult lookupResult = new DictionaryLookupResult(aWord);
-        Scanner scanner = new Scanner(inputStream);
 
-        for (int recNumber = 0; scanner.hasNextLine(); recNumber++) {
-            String line = scanner.nextLine();
-
-            if (recNumber == 0) //Skip over first line in file which is the header
-                continue;
-
-            if (recNumber % 5000 == 0)
-                System.out.println("Searched: " + recNumber + " words...");
-
-            String wordInDictionary = line.substring(line.indexOf(FIELD_DELIMITER) + 1);
-
-            if (aWord.equals(wordInDictionary)) {
-                lookupResult.setIsValidWord(true);
-                System.out.println("FOUND: " + aWord + " at " + recNumber);
-                break;
-            }
+        if (cache.indexOf(' ' + aWord + ' ') > -1) {
+            lookupResult.setIsValidWord(true);
+            System.out.println("FOUND in Dictionary: " + aWord);
+        } else {
+            System.out.println("DID NOT FIND in Dictionary: " + aWord);
         }
 
         return lookupResult;
